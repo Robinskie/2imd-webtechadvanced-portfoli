8f841b6d9db4261fd8050d912b2a637f1ef1301a
Robbe Huybrechts <robbe.huybrechts@hotmail.com>
2021-04-04 22:24:20 +0200

    fix: class names causing problems with adblocker

--- lab4 - apis/App.js
+++ lab4 - apis/App.js
@@ -1,5 +1,7 @@
 const weatherOutput = document.querySelector("#weatherOutput");
 const weatherComment = document.querySelector("#weatherComment");
+const cardImage = document.querySelector("#cardImage");
+const adBackground = document.querySelector(".adv-background");
 
 class App {
     constructor() {
@@ -33,41 +35,47 @@ class Location {
 class Weather {
     constructor(latitude, longitude) {
         const key = "3e49109d71084c3334850b2ebad410f5";
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`)
             .then(response => response.json())
             .then(data => {
                 this.data = data;
 
-                console.log(this.getTemperature());
-
                 let weatherOutputText = "";
                 let weatherCommentText = "";
+                let cardLand = "";
                 // Temperatures
                 if(this.getTemperature() < 10) {
                     weatherOutputText = "a little chilly";
                     weatherCommentText = "enjoying the cold";
+                    cardLand = "plains";
                 } else if(this.getTemperature() < 20) {
                     weatherOutputText = "like nice spring weather";
                     weatherCommentText = "enjoying a picnic";
+                    cardLand = "forest";
                 } else {
                     weatherOutputText = "a little hot";
                     weatherCommentText = "enjoying the scorching heat";
+                    cardLand = "mountain";
                 }
 
                 // Overcast
                 if(this.getState() == "Clouds") {
                     weatherOutputText = "a little overcast";
                     weatherCommentText = "cloudgazing";
+                    cardLand = "swamp";
                 }
 
                 // Raining
                 if(this.getState() == "Thunderstorm" || (this.getState() == "Drizzle") || (this.getState() == "Rain")) {
-                    weatherOutputText = "rainy";
+                    weatherOutputText = "a little rainy";
                     weatherCommentText = "watching the raindrops";
+                    cardLand = "island";
                 }
 
                 weatherOutput.innerHTML = weatherOutputText;
                 weatherComment.innerHTML = weatherCommentText;
+
+                const card = new Card(cardLand);
             });
     }
 
@@ -77,6 +85,18 @@ class Weather {
 
     getState() {
         return this.data.weather[0].main;
+    }
+}
+
+class Card {
+    constructor(input) {
+        fetch(`https://api.scryfall.com/cards/search?q=${input}%20t:land&unique=art`)
+            .then(response => response.json())
+            .then(cardsList => {
+                let pickedCard = cardsList.data[Math.floor(Math.random() * cardsList.data.length)];
+                cardImage.src = pickedCard.image_uris.png;
+                adBackground.style.backgroundImage = `url(${pickedCard.image_uris.png})`;
+            });
     }
 }
 
